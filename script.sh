5905b4d9baad549ea722cebdb744a9bc08947afc
paulaperazzo <mpps@cin.ufpe.br>
2024-11-07 17:42:53 -0300

    feat/cnn-anon: har anon training

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=HAR-anon-optimization
+#SBATCH --job-name=HAR-anon-training
 #SBATCH --ntasks=1
 #SBATCH --mem 16G
 #SBATCH --gres=gpu:2
 #SBATCH -c 8
 #SBATCH -o job.log
-#SBATCH --output=job_output_har_anon_opt_2024-11-07.txt
-#SBATCH --error=job_error_har_anon_opt_2024-11-07.txt
+#SBATCH --output=job_output_har_anon_training_2024-11-07.txt
+#SBATCH --error=job_error_har_anon_training_2024-11-07.txt
 
 # carregar versão python
 module load Python/3.9
@@ -15,4 +15,4 @@ module load Python/3.9
 source ./env/bin/activate
 
 # executar .py
-python3 src/training/optimizer.py
+python3 src/training/main.py
--- src/training/main.py
+++ src/training/main.py
@@ -16,7 +16,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from src.training.mailer import send_email
 
 # Load data
-df = pd.read_csv("processed_train_data_weight.csv")
+df = pd.read_csv("processed_train_data_activity_anonymized.csv")
 
 # Adjust data
 X_acc_x, X_acc_y, X_acc_z, X_gyro_x, X_gyro_y, X_gyro_z, class_labels = matrix_fourier_adjust(df)
@@ -24,7 +24,8 @@ X_acc_x, X_acc_y, X_acc_z, X_gyro_x, X_gyro_y, X_gyro_z, class_labels = matrix_f
 
 # parameters
 input_shape = (256, 1) # shape of the input data
-num_classes = 3 # number of classes
+# num_classes = 3 # number of classes
+num_classes = 6 # number of classes act
 
 input_acc_x, branch_acc_x = create_branch(input_shape)
 input_acc_y, branch_acc_y = create_branch(input_shape)
@@ -36,7 +37,7 @@ input_gyro_z, branch_gyro_z = create_branch(input_shape)
 # concatenate branches
 merged = Concatenate()([branch_acc_x, branch_acc_y, branch_acc_z, branch_gyro_x, branch_gyro_y, branch_gyro_z])
 
-best_hyperparameters = "./src/training/hyperparams/best_hyperparameters_weight.json"
+best_hyperparameters = "./src/anonymization/hyperparams/best_hyperparameters_activity.json"
 with open(best_hyperparameters, 'r') as f:
     best_hyperparameters = json.load(f)
 
@@ -50,7 +51,7 @@ model = Model(inputs=[input_acc_x, input_acc_y, input_acc_z, input_gyro_x, input
 model.compile(optimizer=Adam(learning_rate=best_hyperparameters['learning_rate']), loss='categorical_crossentropy', metrics=['accuracy'])
 print("Model created")
 
-y = tf.keras.utils.to_categorical(class_labels, num_classes=3)
+y = tf.keras.utils.to_categorical(class_labels, num_classes=6)
 
 # Split data train / val
 X_train_acc_x, X_val_acc_x, X_train_acc_y, X_val_acc_y, X_train_acc_z, X_val_acc_z, \
@@ -79,7 +80,7 @@ final_time = time.time()
 # time spent for training
 time_spent = final_time - initial_time
 
-with open("log_time_training_weight.txt", "w") as f:
+with open("log_time_training_act_anon.txt", "w") as f:
     f.write(str(time_spent))
 
 print("Training finished")
@@ -87,7 +88,7 @@ print("Time spent for training: ")
 print(final_time)
 
 # Save model
-model.save("weight_classifier.h5")
+model.save("HAR_anon.h5")
 
 # Send email
-send_email("training weight classifier model")
+send_email("training HAR anonymized model")
